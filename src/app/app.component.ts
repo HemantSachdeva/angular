de215297e43cae85d627371ebeea39314c1e1369
Hemant Sachdeva <hemant.evolver@gmail.com>
2024-01-07 20:37:18 +0530

    hemi-fy: Meta tgs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,11 @@ export class AppComponent implements OnInit{
     this.languageService.initLanguage()
 
 
-    this.titleService.setTitle( "José Andrés | Frontend Developer" );
+    this.titleService.setTitle( "Hemant Sachdeva | Backend Developer" );
 
     this.metaService.addTags([
-      {name: 'keywords', content: 'Frontend, software, developer'},
-      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
+      {name: 'keywords', content: 'Backend, software, developer'},
+      {name: 'description', content: 'With a dynamic 4-year journey in tech, I\'m a passionate coder and solution architect with a trailblazing journey from delivering global production products during college to navigating the corporate realm as an Associate Software Engineer.'},
     ]);
     
     
